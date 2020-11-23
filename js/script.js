5bbf884bad2722ee8edaaf26ad82a93b69006a4a
visuraprabod <visuraprabod@gmail.com>
2020-11-23 16:14:40 +0530

    add countdown animation to begining

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ $('textarea').text('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Co
 
 
 
+
 var text = $('textarea').val();
 
 var words = text.split(" ");
@@ -35,6 +36,21 @@ function output(event){
         console.log('space');
     }*/
 
+    $('main').children('h1:last-child').remove();
+
+
+    $('#type').removeClass('is-invalid');
+
+
+    var regex=('/^'+words[i]+'$/');
+    if(!regex.match($('#type').val())){
+        $('#type').addClass('is-invalid');
+    }
+
+
+
+
+
     if(bool){
         if(event.originalEvent.data===" "){  //This will check whether space is pressed
             $('#type').val('');
@@ -44,12 +60,16 @@ function output(event){
                 $('#type').attr('placeholder','');  //This will clear the placeholder after first word type correctly.
             }
 
+            $('#type').removeClass('is-invalid');
+
             typedTotalChars+=words[i].length;
             setProgressBar();
             i++;
         }
     }
 
+
+    bool=false;
 
 
     if($('#type').val()===words[i]){
@@ -60,22 +80,65 @@ function output(event){
 
 
 
-/*value=0;
-setInterval(function (){
-    setProgressBar(value);
-    value+=10;
-},1000);*/
-
 
 
 
 
 function setProgressBar(){
 
-    var value=Math.ceil(typedTotalChars/totolChars*100)-1;
+    var value=Math.ceil(typedTotalChars/totolChars*100);
 
     $('.progress-bar').css('width',value+'%');
 }
+
+
+$("#type").prop("disabled", true);
+
+
+
+setTimeout(function (){
+    $("#type").prop("disabled", false);
+    $('#type').focus();
+},3000);
+
+
+
+
+
+
+var intId;
+
+var j=1;
+intId=setInterval(function (){
+    switch (j){
+
+        case 1:$('#countdown').children('div:first-child').addClass('invisible');
+            $('#countdown').children('div:nth-child(2)').removeClass('invisible');
+            break;
+        case 2:$('#countdown').children('div:nth-child(2)').addClass('invisible');
+            $('#countdown').children('div:nth-child(3)').removeClass('invisible');
+            break;
+
+        case 3:$('#countdown').children('div:nth-child(3)').addClass('invisible');
+                $('main').append($('<h1 id="go" style="color: red; font-size: 60px" class="animate__animated animate__zoomIn">GO!!</h1>'))
+                break;
+    }
+
+    j++;
+
+},1000);
+
+
+
+
+setTimeout(function (){
+    clearInterval(intId);
+
+},3000);
+
+
+
+
 
 
 
